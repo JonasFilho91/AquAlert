65d3ee3f6fb53051732d1004520ba7e589a6783f
JonasFilho91 <jonas16051991@gmail.com>
2025-05-28 18:32:16 -0300

    Update - app - cors v1

--- app.js
+++ app.js
@@ -4,145 +4,135 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const Longin = require('./src/crud/Login')
-<<<<<<< HEAD
 const bd = require('./src/bd');
-=======
-const bd = require('./src/bd.js')
 const cors = require('cors');
-
-// Configuração padrão, permitindo todas as origens
-app.use(cors());
-
-app.use(cors(corsOptions));
->>>>>>> f60c75995311f7e9815b696583930048799aa650
-
-
-//Conecta ao BD
-bd.conectarBd()
 
 //Configuração do Express
 const app = express();
 
+//Conecta ao BD
+bd.conectarBd()
+
+// Configurando o CORS
+app.use(cors());
+
 //Configuração JSON response
-
-//Funcao de validacao de email. caso seja invalido, interrompe o envio.
-function validarEmail(email) {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-}
-
 app.use(express.json());
+
+//Configuração Porta
+const port = 3000;
 
 //Rotas Publicas
 
-  //Rota de Registro - Cria um novo usuário
-  app.post('/auth/registro', async (req, res) => {
+//Rota de Registro - Cria um novo usuário
+app.post('/auth/registro', async (req, res) => {
 
-    //Recebe os dados do corpo   
-    const { nome, email, senha, confirmarSenha } = req.body
-    //Verifica se todos os campos foram preenchidos
-    if (!nome || !email || !senha || !confirmarSenha) {
-      return res.status(422).json({ error: 'Todos os campos são obrigatórios' })
-    } else if (senha != confirmarSenha) {
-      return res.status(422).json({ error: 'As senhas devem ser iguais' })}
+  //Recebe os dados do corpo   
+  const { nome, email, senha, confirmarSenha } = req.body
+  //Verifica se todos os campos foram preenchidos
+  if (!nome || !email || !senha || !confirmarSenha) {
+    return res.status(422).json({ error: 'Todos os campos são obrigatórios' })
+  } else if (senha != confirmarSenha) {
+    return res.status(422).json({ error: 'As senhas devem ser iguais' })
+  }
 
-      // Valida o e-mail
+  // Valida o e-mail
   if (!validarEmail(email)) {
     return res.status(422).json({ error: 'E-mail inválido' });
   }
-    //Cria o usuário no BD
-    else {
-      //Cria password
-      const salt = await bcrypt.genSalt(12);
-      const passwordHash = await bcrypt.hash(senha, salt);
+  //Cria o usuário no BD
+  else {
+    //Cria password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(senha, salt);
 
-  
-      let cod = await Longin.CreateUsuario({nome, email, passwordHash});
-      console.log(cod);
-      //Usuario ja cadastrado
-      if (cod == 1) {
-        console.log('Usuário ', nome, ' já cadastrado!');
-        res.status(422).json({ msg: 'Usuário ' + nome + ' já cadastrado!' })
-      }
-      //Sucesso
-      else if (cod == 2) {
-        console.log('Usuário ' + nome + ', salvo com sucesso!');
-        res.status(201).json({ msg: 'Usuário ' + nome + ', salvo com sucesso!' })
-      }
-      //Erro
-      else if (cod == 3) {
-        console.log('Erro ao salvar novo Usuário, verifique as informações ou contate o suporte!');
-        res.status(500).json({ msg: 'Erro ao salvar novo Usuário, verifique as informações ou contate o suporte!' })
-      }
+
+    let cod = await Longin.CreateUsuario({ nome, email, passwordHash });
+    console.log(cod);
+    //Usuario ja cadastrado
+    if (cod == 1) {
+      console.log('Usuário ', nome, ' já cadastrado!');
+      res.status(422).json({ msg: 'Usuário ' + nome + ' já cadastrado!' })
     }
-  })
-  //Rota de Login
-  app.post('/auth/login', async (req, res) => {
-
-    //Recebe os dados do corpo   
-    const { email, senha } = req.body
-    //Verifica se todos os campos foram preenchidos
-    if (!email || !senha) {
-      return res.status(422).json({ error: 'Todos os campos são obrigatórios' })
+    //Sucesso
+    else if (cod == 2) {
+      console.log('Usuário ' + nome + ', salvo com sucesso!');
+      res.status(201).json({ msg: 'Usuário ' + nome + ', salvo com sucesso!' })
     }
-    else {
-
-      // Valida o e-mail
-  if (!validarEmail(email)) {
-    return res.status(422).json({ error: 'E-mail inválido' });
+    //Erro
+    else if (cod == 3) {
+      console.log('Erro ao salvar novo Usuário, verifique as informações ou contate o suporte!');
+      res.status(500).json({ msg: 'Erro ao salvar novo Usuário, verifique as informações ou contate o suporte!' })
+    }
   }
-      //Checa se o usuário já é cadastrado
-      const UsuarioExistente = (await Longin.ConsultarUsuarioExistente(email));
+})
+//Rota de Login
+app.post('/auth/login', async (req, res) => {
 
-      //Usuario não cadastrado
-      if (!UsuarioExistente.Resultado) {
-        console.log('Usuário ', email, ' não cadastrado!');
-        res.status(404).json({ msg: 'Usuário ' + email + ' nao cadastrado!' })
-      
-      }
-      //checa senha
-      else if (!await bcrypt.compare(senha, UsuarioExistente.userSenha)) {
-        console.log('Senha incorreta!');
-        res.status(422).json({ msg: 'Senha incorreta!' })
-      }
-      //Sucesso
-      else {
-        try{
-          const secret = process.env.SECRET;
-          const userId = UsuarioExistente.userId
-          const token = jwt.sign(
-            {
+  //Recebe os dados do corpo   
+  const { email, senha } = req.body
+  //Verifica se todos os campos foram preenchidos
+  if (!email || !senha) {
+    return res.status(422).json({ error: 'Todos os campos são obrigatórios' })
+  }
+  else {
+
+    // Valida o e-mail
+    if (!validarEmail(email)) {
+      return res.status(422).json({ error: 'E-mail inválido' });
+    }
+    //Checa se o usuário já é cadastrado
+    const UsuarioExistente = (await Longin.ConsultarUsuarioExistente(email));
+
+    //Usuario não cadastrado
+    if (!UsuarioExistente.Resultado) {
+      console.log('Usuário ', email, ' não cadastrado!');
+      res.status(404).json({ msg: 'Usuário ' + email + ' nao cadastrado!' })
+
+    }
+    //checa senha
+    else if (!await bcrypt.compare(senha, UsuarioExistente.userSenha)) {
+      console.log('Senha incorreta!');
+      res.status(422).json({ msg: 'Senha incorreta!' })
+    }
+    //Sucesso
+    else {
+      try {
+        const secret = process.env.SECRET;
+        const userId = UsuarioExistente.userId
+        const token = jwt.sign(
+          {
             id: userId
-            }, 
-            secret);
+          },
+          secret);
 
-            res.status(200).json({ msg: 'Usuário ' + email + ', logado com sucesso!', token, user: userId });
+        res.status(200).json({ msg: 'Usuário ' + email + ', logado com sucesso!', token, user: userId });
 
-          console.log(token);
-0
-        }catch(error){
-          console.log('Erro ao logar Usuário, verifique as informações ou contate o suporte!');
-          res.status(500).json({ msg: 'Erro ao logar Usuário, verifique as informações ou contate o suporte!' })
-        }
+        console.log(token);
+        0
+      } catch (error) {
+        console.log('Erro ao logar Usuário, verifique as informações ou contate o suporte!');
+        res.status(500).json({ msg: 'Erro ao logar Usuário, verifique as informações ou contate o suporte!' })
       }
     }
-  })
+  }
+})
 
-  //Rotas Privadas
+//Rotas Privadas
 
-    //Rota Salvar Dados
-    app.get('/user/dados', checkToken, async (req, res) => {
-      res.status(200).json({ msg: 'Dados salvos com sucesso!' })
-    })
+//Rota Salvar Dados
+app.get('/user/dados', checkToken, async (req, res) => {
+  res.status(200).json({ msg: 'Dados salvos com sucesso!' })
+})
 
-    //Rota deletar Usuário
-    app.delete('/user/deletaruser', checkToken, async (req, res) => {
+//Rota deletar Usuário
+app.delete('/user/deletaruser', checkToken, async (req, res) => {
 
-      const {userId} = req.body
-      const result = await Longin.DeletarUser(userId)
-      res.status(result.status).json({ msg: result.msg })
-      
-    })
+  const { userId } = req.body
+  const result = await Longin.DeletarUser(userId)
+  res.status(result.status).json({ msg: result.msg })
+
+})
 
 /**
  * Middleware responsavel por verificar se o token
@@ -150,21 +140,29 @@ app.use(express.json());
  * a rotas privadas.
  */
 function checkToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) {
-      return res.status(401).json({ msg: 'Acesso negado' });
-    }
-      try{
-        const secret = process.env.SECRET;
-        jwt.verify(token, secret);
-        next();
-
-        }catch(error){
-          return res.status(403).json({ msg: 'Token invalido' });
-
-        } 
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) {
+    return res.status(401).json({ msg: 'Acesso negado' });
   }
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+
+  } catch (error) {
+    return res.status(403).json({ msg: 'Token invalido' });
+
+  }
+}
+
+//Funcao de validacao de email. caso seja invalido, interrompe o envio.
+function validarEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
 
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
